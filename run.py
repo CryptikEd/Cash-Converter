407c19282c51f6057fc8388478ed5c3941b312f6
Edwin Kigen <kubwayokigen@gmail.com>
2024-05-15 18:33:13 +0000

    Edited welcome message.

--- run.py
+++ run.py
@@ -12,21 +12,24 @@ print(logo)
 
 # Welcome message and instructions for the user.
 print("Welcome to the currency converter made by CryptikEd.")
-print("This converter can convert all of the world's currencies.")
-print("Please enter the following information under.")
+print("This converter can convert most of the world's currencies.")
+print("Please follow the instructions below.")
+print("Use numerical values without decimals (e.g., 100)")
+print("Write the currency using currency code (e.g., USD)")
 
 
 def my_cash_converter():
     while True:
         # Prompt user to input the amount and currencies.
         while True:
-            amount_str = input("Enter the amount to convert: ")
+
+            amount_str = input("Enter the amount to convert:\n")
             # Check if the input string contains only digits.
             if amount_str.isdigit():
                 break
 
             else:
-                print("Invalid input: Please enter a valid number.")
+                print("Invalid input: Please enter a valid numerical value.")
 
         from_currency = input(
             "Enter the currency to convert from (e.g., USD, EUR, GBP): "
@@ -71,7 +74,7 @@ def my_cash_converter():
         except ValueError:
 
             # Handle invalid input for the amount.
-            print("Invalid input: The amount must be a valid number.")
+            print("Invalid input: The amount must be a valid currency code.")
         except Exception as e:
             # Handle other exceptions such as network errors.
             print(f"An error occurred: {e}")
