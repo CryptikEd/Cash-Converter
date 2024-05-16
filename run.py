30bfd69a185b9d967d8087b90c1baca22320864e
Edwin Kigen <kubwayokigen@gmail.com>
2024-05-16 16:30:26 +0000

    Error handling E261.

--- run.py
+++ run.py
@@ -57,7 +57,7 @@ def my_cash_converter():
                 print(
                     f"Conversion from {from_currency} to {to_currency} "
                     f"is not supported."
-                )# Check if conversion rate is available
+                )  # Check if conversion rate is available
             elif "conversion_rate" in data:
                 # Extract conversion rate from the response.
                 conversion_rate = data["conversion_rate"]
